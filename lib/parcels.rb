de1e5b5d55ad3317313ff6034630d497d7e069be
Chris Clifford and Ezra Sandzer-Bell <ezra@>
2016-11-28 11:06:53 -0800

    add method to calculate shipping cost

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,12 +1,42 @@
 class Parcel
-  define_method(:initialize) do |length, width, height, weight|
+  define_method(:initialize) do |length, width, height, weight, distance, speed|
     @length = length
     @width = width
     @height = height
     @weight = weight
+    @distance = distance
+    @speed = speed
   end
 
   define_method(:volume) do
     @length * @width * @height
+  end
+
+  define_method(:cost_to_ship) do
+    # cost per pound = $1.50
+    # service charge of $0.50 for each cubic foot beyond 2 cubic feet
+    # 1 cubic foot = 12 * 12 * 12 = 1728
+    # distance represented in miles
+    # cost of distance is set at $1 per 1000 miles
+    # soeed 1 = express shipping, 2 = standard
+    weight_cost = @weight*1.5
+    volume_cost = 0
+    cubic_feet = self.volume()/1728.0
+    if cubic_feet > 2
+      minus_two = cubic_feet-2
+      while minus_two > 0
+        volume_cost += 0.5
+        minus_two -= 1
+      end
+      volume_cost
+    end
+    distance_cost = 2 + @distance/500
+    if @speed == 1
+      speed_cost = 5
+    else
+      speed_cost = 1
+    end
+    total_cost = weight_cost + volume_cost + distance_cost + speed_cost
+    total_cost.round(2)
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,11 +1,24 @@
 require('parcels')
 require('rspec')
+require('pry')
 
 describe(Parcel) do
   describe('#volume') do
     it('return the volume of a parcel object') do
-      new_parcel = Parcel.new(10, 10, 10, 5)
+      new_parcel = Parcel.new(10, 10, 10, 5, 0, 0)
       expect(new_parcel.volume).to(eq(1000))
+    end
+  end
+
+# set volume at 4000 cubic inches (1.157 cubic ft) = .314 = $0.50
+# set weight at 5 lb = $7.5
+# set distance at 2500 miles = $7.00
+# set speed to express = $5
+
+  describe('#cost_to_ship') do
+    it('evaluates the cost to ship based on formula') do
+      parcel_cost = Parcel.new(20, 20, 10, 5, 2500, 1)
+      expect(parcel_cost.cost_to_ship).to(eq(20.00))
     end
   end
 end
